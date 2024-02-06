9928a6bcc7e1ff7241962988e8f93fad8325808a
Angelinero48 <116581341+Angelinero48@users.noreply.github.com>
2024-02-06 17:23:29 +0100

    map json

--- empleados.py
+++ empleados.py
@@ -9,27 +9,36 @@ with open('config.json', 'r') as file:
 app = Flask(__name__)
 
 @app.route("/empleadosLista")
-
 def obtenerLista():
+    mydb = mysql.connector.connect(
+        host=config_data['mydb']['host'],
+        user=config_data['mydb']['user'],
+        database=config_data['mydb']['database']
+    )
 
-  mydb = mysql.connector.connect(
-    host=config_data['mydb']['host'],
-    user=config_data['mydb']['user'],
-    database=config_data['mydb']['database']
-)
-  mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-  listaEmpleados = "SELECT * FROM empleados"
+    # Ejecutar una consulta SELECT
+    mycursor.execute("SELECT * FROM empleados")
 
-  mycursor.execute(listaEmpleados)
+    # Recuperar todas las filas del resultado
+    myresult = mycursor.fetchall()
 
-  myresult = mycursor.fetchall()
-  
-  # convert into JSON:
-  y = json.dumps(myresult)
+    # Verificar si se obtuvieron resultados
+    if myresult:
+        # Obtener los nombres de las columnas
+        column_names = [i[0] for i in mycursor.description]
 
-  # the result is a JSON string:
-  return y
+        # Crear una lista de diccionarios, cada uno representando a un empleado
+        employees_dict_list = [
+            {column_names[i]: row[i] for i in range(len(column_names))}
+            for row in myresult
+        ]
+
+        # Convertir la lista de diccionarios en JSON y devolverla
+        return json.dumps(employees_dict_list)
+    else:
+        return "No se encontraron resultados."
 
 @app.route("/empleadosNuevo")
 
